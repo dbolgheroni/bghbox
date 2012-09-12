2321d3a2b480dd6721a9a5dbbb6f5ae632ee8954
Daniel Bolgheroni <dbolgheroni@gmail.com>
2012-09-11 22:45:18 -0300

    Exercise 2-1.

---
+++ book/accelerated_cpp/2-1/2-1.cpp
@@ -0,0 +1,48 @@
+/* 2-1 */
+
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+    cout << "Please enter your first name: ";
+
+    string name;
+    cin >> name;
+
+    const string greeting = "Hello, " + name + "!";
+
+    // the change is here
+    const int pad = 0;
+
+    const int rows = pad * 2 + 3;
+    const string::size_type cols = greeting.size() + pad * 2 + 2;
+
+    cout << endl;
+
+    for (int r = 0; r != rows; ++r) {
+        string::size_type c = 0;
+
+        while (c != cols) {
+            if (r == pad + 1 && c == pad + 1) {
+                cout << greeting;
+                c += greeting.size();
+            } else {
+                if (r == 0 || r == rows -1 ||
+                    c == 0 || c == cols -1)
+                    cout << "*";
+                else
+                    cout << " ";
+                ++c;
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
