57ff9b87302ce097670afc573a1c4082e8064c41
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-02-02 17:18:54 -0200

    Use onload instead of checking state explicitly.

--- cookbook/js/xmlhttprequest-json/script.js
+++ cookbook/js/xmlhttprequest-json/script.js
@@ -5,16 +5,11 @@ var req = new XMLHttpRequest();
 req.open("GET", "note.json");
 req.responseType = "json";
 req.send();
-req.onreadystatechange = function () {
-    console.log("readyState: %d", req.readyState);
+req.onload = function () {
+    response = req.response;
 
-    /* check to see if it's done, check http://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest */
-    if (req.readyState == 4) {
-        response = req.response;
-
-        document.getElementById("to").innerHTML = response.to;
-        document.getElementById("from").innerHTML = response.from;
-        document.getElementById("message").innerHTML = response.body;
-    }
+    document.getElementById("to").innerHTML = response.to;
+    document.getElementById("from").innerHTML = response.from;
+    document.getElementById("message").innerHTML = response.body;
 };
 });
