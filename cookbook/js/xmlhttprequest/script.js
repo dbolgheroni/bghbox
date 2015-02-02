0a100b9a59846385f8d794bd391b7a13155a6863
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-02-02 17:13:53 -0200

    Does not need to check for readyState explicitly.

--- cookbook/js/xmlhttprequest/script.js
+++ cookbook/js/xmlhttprequest/script.js
@@ -4,16 +4,12 @@ var req = new XMLHttpRequest();
 
 req.open("GET", "note.xml");
 req.send();
-req.onreadystatechange = function () {
-    console.log("readyState: %d", req.readyState);
 
-    /* check to see if it's done, check http://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest */
-    if (req.readyState == 4) {
-        xmlDoc = req.responseXML;
+req.onload = function () {
+    xmlDoc = req.responseXML;
 
-        document.getElementById("to").innerHTML = xmlDoc.getElementsByTagName("to")[0].childNodes[0].nodeValue;
-        document.getElementById("from").innerHTML = xmlDoc.getElementsByTagName("from")[0].childNodes[0].nodeValue;
-        document.getElementById("message").innerHTML = xmlDoc.getElementsByTagName("body")[0].childNodes[0].nodeValue;
-    }
+    document.getElementById("to").innerHTML = xmlDoc.getElementsByTagName("to")[0].childNodes[0].nodeValue;
+    document.getElementById("from").innerHTML = xmlDoc.getElementsByTagName("from")[0].childNodes[0].nodeValue;
+    document.getElementById("message").innerHTML = xmlDoc.getElementsByTagName("body")[0].childNodes[0].nodeValue;
 };
 });
