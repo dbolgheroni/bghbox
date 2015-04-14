2df98c499c8ec8305231125286bac1d35e165ea5
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-04-14 18:38:51 -0300

    Indentation.

--- cookbook/js/understanding_scope/script.js
+++ cookbook/js/understanding_scope/script.js
@@ -1,22 +1,24 @@
 /* run only after page is loaded */
 $(function() {
-    console.log("page loaded, executing script"); 
-    var x = 1;
+console.log("page loaded, executing script"); 
 
-    var func1 = function () {
-        var x = 2;
-        console.log("inside func1: ", x);
+var x = 1;
+
+var func1 = function () {
+    var x = 2;
+    console.log("inside func1: ", x);
 
 
-        var func11 = function() {
-            var x = 3;
-            console.log("inside func11 in func1: ", x);
-        }
-
-        func11();
-        console.log("inside func1: ", x);
+    var func11 = function() {
+        var x = 3;
+        console.log("inside func11 in func1: ", x);
     }
 
-    console.log("outside func1: ", x);
-    func1();
+    func11();
+    console.log("inside func1: ", x);
+}
+
+console.log("outside func1: ", x);
+func1();
+
 });
