2058f849a34800aaa67135179c1c1ffb04494466
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-04-28 22:12:54 -0300

    Do not need XMLHttpRequest when using jQuery.

--- cookbook/js/ws-client-jquery/script.js
+++ cookbook/js/ws-client-jquery/script.js
@@ -2,8 +2,6 @@
 $(function() {
 console.log("page loaded, executing script"); 
 
-var req = new XMLHttpRequest();
-
 $.getJSON("http://localhost:5000/todo/api/v1.0/tasks/1", function (response) {
     $("#id").html(response.task.id);
     $("#title").html(response.task.title);
