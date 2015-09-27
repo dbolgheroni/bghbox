25aac7c06e05f0055ae539147936fe2745480645
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-09-27 12:37:26 -0300

    Fix loop that runs beyond the arrays limits.
    Some minor variable/macro renaming.

--- electronics/arduino/rswtch/rswtch.ino
+++ electronics/arduino/rswtch/rswtch.ino
@@ -13,17 +13,14 @@
  * Arduino reference is at /usr/local/share/doc/arduino/reference/
  */
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define BOARDLED 13
-#define PS1 12
-#define PS2 11
-#define PS3 10
-
-#define NCMD 32
+#define CH1 12
+#define CH2 11
+#define CH3 10
 
 String inputStr = "";
 boolean strComplete = false;
@@ -59,32 +56,36 @@ struct cmd {
     int ch;
     void (*func)(int ch);
 } cmdlist[] = {
-    { "u1", PS1, up_channel     },
-    { "u2", PS2, up_channel     },
-    { "u3", PS3, up_channel     },
-    { "d1", PS1, down_channel   },
-    { "d2", PS2, down_channel   },
-    { "d3", PS3, down_channel   },
-    { "t1", PS1, toggle_channel },
-    { "t2", PS2, toggle_channel },
-    { "t3", PS3, toggle_channel },
-    { "r1", PS1, reset_channel  },
-    { "r2", PS2, reset_channel  },
-    { "r3", PS3, reset_channel  }
+    { "u1", CH1, up_channel     },
+    { "u2", CH2, up_channel     },
+    { "u3", CH3, up_channel     },
+    { "d1", CH1, down_channel   },
+    { "d2", CH2, down_channel   },
+    { "d3", CH3, down_channel   },
+    { "t1", CH1, toggle_channel },
+    { "t2", CH2, toggle_channel },
+    { "t3", CH3, toggle_channel },
+    { "r1", CH1, reset_channel  },
+    { "r2", CH2, reset_channel  },
+    { "r3", CH3, reset_channel  }
 };
+
+int ncmd = sizeof(cmdlist)/sizeof(struct cmd);
 
 void setup(void) {
     /* initialize pins */
     pinMode(BOARDLED, OUTPUT);
-    pinMode(PS1, OUTPUT);
-    pinMode(PS2, OUTPUT);
+    pinMode(CH1, OUTPUT);
+    pinMode(CH2, OUTPUT);
+    pinMode(CH3, OUTPUT);
 
     /* turn off board led, use it to debug error */
     digitalWrite(BOARDLED, LOW);
 
     /* on by default */
-    digitalWrite(PS1, HIGH);
-    digitalWrite(PS2, HIGH);
+    digitalWrite(CH1, HIGH);
+    digitalWrite(CH2, HIGH);
+    digitalWrite(CH3, HIGH);
 
     /* initialize serial code */
     Serial.begin(115200);
@@ -93,7 +94,7 @@ void setup(void) {
     delay(1000);
 
     /* present itself */
-    Serial.println("rswtch");
+    Serial.println("Bolgh rswtch");
     Serial.print("> ");
 
     inputStr.reserve(72);
@@ -102,13 +103,13 @@ void setup(void) {
 }
 
 void loop(void) {
-    boolean found = 0;
+    boolean found = false;
     serialEvent();
 
     if (strComplete) {
         /* check if the command exists and call the appropriate func if
          * found, passing the channel number as argument */
-        for (int i = 0; i < NCMD; i++) {
+        for (int i = 0; i < ncmd; i++) {
             if (cmdlist[i].name + '\r' == inputStr) {
                 (*cmdlist[i].func)(cmdlist[i].ch);
                 found = true;
