8e68552d59619a1a2cfd2c7fe24ee97014a79d80
Daniel Bolgheroni <dbolgheroni@gmail.com>
2015-09-26 21:44:15 -0300

    Use my "official" test IP.

--- electronics/arduino/UIPEthernet-UdpServer/UIPEthernet-UdpServer.ino
+++ electronics/arduino/UIPEthernet-UdpServer/UIPEthernet-UdpServer.ino
@@ -47,7 +47,7 @@ void setup() {
 
   uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
 
-  Ethernet.begin(mac,IPAddress(192,168,1,252),53);
+  Ethernet.begin(mac,IPAddress(192,168,1,253),53);
 
   int success = udp.begin(5000);
 
